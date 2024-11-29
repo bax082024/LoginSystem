8480b86a4ce6d9f9ac01dabe306d5caf048911e4
bax082024 <bax082024@gmail.com>
2024-11-29 14:34:29 +0100

    added menu

--- Program.cs
+++ Program.cs
@@ -7,6 +7,19 @@ namespace LoginSystem
 {
     class Program
     {
+        static Dictionary<string, string> userDatabase = new Dictionary<string, string>();
 
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1. Register");
+                Console.WriteLine("2. Login");
+                Console.WriteLine("3. Exit");
+                Console.Write("Select an option: ");
+                string choice = Console.ReadLine();
+            }
+        }
     }
 }
